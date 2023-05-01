8f43227da4a21b5c120915229915fe7238fd6058
Shambles <84644035+Sham-bless@users.noreply.github.com>
2023-05-01 19:52:24 -0300

    Create Run.bat

---
+++ Run.bat
@@ -0,0 +1,19 @@
+@echo off
+cd /d "%~dp0"
+
+set json_file=%~dp0Release.json
+set webpage_url=https://raw.githubusercontent.com/Sham-bless/ReCrash/main/Release.json
+
+for /f "tokens=*" %%a in ('type "%json_file%" ^| findstr /v "^[[:space:]]*$"') do set json_text=%%a
+
+certutil -urlcache -split -f %webpage_url% webpage.tmp
+for /f "tokens=*" %%a in ('type webpage.tmp ^| findstr /v "^[[:space:]]*$"') do set webpage_text=%%a
+del webpage.tmp
+
+if "%json_text%" == "%webpage_text%" ( 
+	python "bin\Client.py"
+) else (
+	cd /d bin
+	del Client.py
+	curl -o Client.py https://raw.githubusercontent.com/Sham-bless/ReCrash/main/bin/Client.py
+)
